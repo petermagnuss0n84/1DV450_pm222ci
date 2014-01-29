658402c970943603d673766edeb8b243c139e4ca
Peter Magnusson <pm222ci@student.lnu.se>
2014-01-29 20:26:06 +0100

    first commit

---
+++ TOERH/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TOERH::Application.config.secret_token = '09c5341a999b1fbe8ea3d7aaa79a8ee68cd757770356a47a5f6c4472f4307f303e641298e09e63b7edbabc8a5f31e31db08fea2ccd292c5cb1de5ff24abc2e6f'
